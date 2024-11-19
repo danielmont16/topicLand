c1c793e3f99ef6ed9ca092f82e066e98d7d01c6a
Daniel <dfelipemont@gmail.com>
2024-11-19 18:57:12 -0500

    login feature

--- configs/globals.js
+++ configs/globals.js
@@ -4,8 +4,20 @@ require("dotenv").config();
 
 const configurations = {
     ConnectionStrings:{
-        MongoDB:"mongodb+srv://dfelipemont:npnYb36x2672O2pE@cluster0.89sr3.mongodb.net/magazineDB?retryWrites=true&w=majority&appName=Cluster0"
+        MongoDB:process.env.DB_CONNECT
+    },
+  Authentication: {
+    GitHub: {
+      ClientId: process.env.GITHUB_CLIENT_ID,
+      ClientSecret: process.env.GITHUB_CLIENT_SECRET,
+      CallbackUrl: process.env.GITHUB_CALLBACK_URL
+    },
+    Google:{
+      ClientId:process.env.GOOGLE_CLIENT_ID,
+      ClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+      CallbackUrl: process.env.GOOGLE_CALLBACK_URL
     }
+  },
 }
 
 module.exports = configurations;
