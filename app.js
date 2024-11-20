d0004a229e1790dc0e8a19e3ee0668055d965463
Carlos Serrano <carlos.serrano.prado@gmail.com>
2024-11-19 21:57:33 -0500

    Pull Login Code to make everything work

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 //adding topics and articles routes
-var topicsRouter = require('./routes/topics');
-var articlesRouter = require('./routes/articles');
+var topicsRouter = require("./routes/topics");
+var articlesRouter = require("./routes/articles");
 
 //Import MongoDB and Configuration modules
 var mongoose = require("mongoose");
@@ -27,14 +27,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 //adding topics and articles
-app.use('/topics', topicsRouter);
-app.use('/articles', articlesRouter);
-
+app.use("/topics", topicsRouter);
+app.use("/articles", articlesRouter);
 
 // Connecting to the DB
 mongoose
@@ -62,25 +61,24 @@ hbs.registerHelper("createOptionElement", (currentValue, selectedValue) => {
   );
 });
 // helper function to format date values
-hbs.registerHelper('toShortDate', (longDateValue) => {
-  return new hbs.SafeString(longDateValue.toLocaleDateString('en-CA'));
+hbs.registerHelper("toShortDate", (longDateValue) => {
+  return new hbs.SafeString(longDateValue.toLocaleDateString("en-CA"));
 });
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
